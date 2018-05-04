ad8c6bd2332ed756668dd9dd7da59b8daf67d84b
Faraaz Nishtar <faraaznishtar@gmail.com>
2018-05-03 19:58:19 -0700

    move hacks into a single file to deal with later

--- pages/index.js
+++ pages/index.js
@@ -7,42 +7,8 @@ import ErrorCard from '../components/errorCard';
 import Runkit from '../components/runkit';
 import FunctionContent from '../components/functionContent';
 import Web3Container from '../lib/web3Container';
-// import data2 from '../data2.js';
-import data2 from './0x.js';
-import contractInformation from './test';
-
-function parseFunctionName(name) {
-  return name.split('(')[0];
-}
-
-const contractFileName = 'TokenRegistry.sol:TokenRegistry';
-const contractAbi = JSON.parse(data2.contracts[contractFileName].abi);
-const devdoc = JSON.parse(data2.contracts[contractFileName].devdoc);
-
-const methodNames = Object.keys(devdoc.methods).map(parseFunctionName);
-
-const steps = contractAbi.map((meth) => {
-  const methodDevdoc = getMethodDevdoc(meth.name, devdoc.methods);
-
-  if (methodDevdoc) {
-    meth.devdoc = methodDevdoc;
-  }
-  return meth;
-}).filter(abi => abi.devdoc).filter(abi => abi.devdoc.details)
-
-
-function getMethodDevdoc(methodName, devDocMethods) {
-  let answer;
-
-  Object.keys(devDocMethods).forEach((key) => {
-    if (parseFunctionName(key) === methodName) {
-      // console.log(`key: ${key}, methodName: ${methodName}`, devDocMethods[key]);
-      answer = devDocMethods[key];
-    }
-  });
-
-  return answer;
-  }
+import steps from '../lib/hacks';
+import contractInformation from './test.js';
 
 class DocumentationCtrl extends React.Component {
   constructor() {

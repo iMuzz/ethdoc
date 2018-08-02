0613c48541a38276bcdd5ff3b08dd7c6fbf3e569
achalvs <avs@achal.me>
2018-08-02 16:26:36 -0700

    add margin-bottom

--- components/simpleCard.js
+++ components/simpleCard.js
@@ -24,6 +24,7 @@ class SimpleCard extends React.Component {
         <style>
           {`
             .simple-card {
+              margin-bottom: 2em;
               background-color: white;
               box-shadow: 0 2px 5px 0 rgba(51,51,79,0.07);
               border-radius: 6px;

8d7b89f571152703f9b05d4640dd54a38f1d0d07
Faraaz Nishtar <faraaznishtar@gmail.com>
2018-08-20 22:45:41 -0700

    Modify next.js config to get `fs` package working

--- next.config.js
+++ next.config.js
@@ -10,5 +10,13 @@ module.exports = {
       '/': { page: '/' },
       '/demo': { page: '/demo' },
     }
-  }
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders }) => {
+    // Perform customizations to webpack config
+    // Important: return the modified config
+    config.node = {
+      fs: "empty"
+    };
+    return config;
+  },
 }

ed0b891ed74ae9f1ddad9d0f907257401d2c782e
Faraaz Nishtar <faraaznishtar@gmail.com>
2018-08-21 14:58:33 -0700

    remove fs dependency

--- next.config.js
+++ next.config.js
@@ -10,13 +10,5 @@ module.exports = {
       '/': { page: '/' },
       '/demo': { page: '/demo' },
     }
-  },
-  webpack: (config, { buildId, dev, isServer, defaultLoaders }) => {
-    // Perform customizations to webpack config
-    // Important: return the modified config
-    config.node = {
-      fs: "empty"
-    };
-    return config;
-  },
+  }
 }

191a9dad8a4a8aeecb1826ab3ca5bfc174a3d037
Faraaz Nishtar <faraaznishtar@gmail.com>
2018-09-24 13:34:55 -0700

    add GA

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,8 @@
 import Document, { Head, Main, NextScript } from 'next/document';
 import flush from 'styled-jsx/server';
 
+import { GA_TRACKING_ID } from '../lib/gtag'
+
 export default class MyDocument extends Document {
 	static getInitialProps({ renderPage }) {
 		const { html, head, errorHtml, chunks } = renderPage();
@@ -44,6 +46,22 @@ export default class MyDocument extends Document {
 						rel="stylesheet"
 					/>
 					<script src="https://embed.runkit.com" />
+
+					{/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${GA_TRACKING_ID}');
+          `}}
+          />
 				</Head>
 				<body>
 					{this.props.customValue}
